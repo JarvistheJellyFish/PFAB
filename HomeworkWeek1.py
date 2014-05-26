3e12a18085c27f9dacb4a1ebe841ee00ac766388
Jarvis <jarvisgamestudio@gmail.com>
2014-05-25 23:27:10 -0400

    Homework for PFAB week 1

---
+++ HomeworkWeek1.py
@@ -0,0 +1,25 @@
+#I'm assuming you don't want try/catch to handle bad input for age
+
+import math
+
+first = raw_input("What is your first name?: ")
+last = raw_input("What is your last name?: ")
+age = input("How old are you?: ")
+
+print "Your name is",first,last,"and you are",age,"years old."
+
+sideA = 12.55
+sideB = 17.85
+sideC = math.sqrt((sideA**2)+(sideB**2))
+
+print "Side C is",sideC,"units long."
+
+print "95 + 64.5 =",95+64.5
+print "95 - 64.5 =",95-64.5
+print "95 * 64.5 =",95*64.5
+print "95 / 64.5 =",95/64.5
+print "95 % 64.5 =",95%64.5
+
+#Im not sure what you are asking, but if you are asking why my formatting is the way it
+#is is because I don't have to find the values myself, I just let the computer do it.
+
